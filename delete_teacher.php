b99473bec5e9e7dcd171f3209757bd2984ae3645
Student <lab-104@aptech-mlr.edu>
2025-07-09 21:00:39 +0500

    new

---
+++ delete_teacher.php
@@ -0,0 +1,17 @@
+<?php
+require_once "config.php";
+
+
+  if(isset($_GET["id"])){
+    $id = $_GET["id"];
+
+    $query ="delete from tbl_teacher where teacher_id = $id";
+    $result =  mysqli_query($connection,$query);
+
+    if($result){
+        header("location:view_teacher.php");
+        exit;
+    }
+     //echo $id;
+  }
+?>
--- view_teacher.php
+++ view_teacher.php
@@ -10,15 +10,16 @@
       <th scope="col">Teacher Name</th>
       <th scope="col">Teacher Email</th>
       <th scope="col">Teacher Batch</th>
-      <th scope="col">Teacher Profile</th>
+      <th scope="col">Teacher Image</th>
+      <th scope="col" >Action</th>
     </tr>
   </thead>
   <tbody>
     <?php 
-        $q ="SELECT tbl_teacher.teacher_id, teacher_name ,teacher_email, tbl_batch.batch_name ,tbl_teacher.teacher_img_path FROM `tbl_teacher` join tbl_batch on tbl_batch.batch_id = tbl_teacher.teacher_batch;";
-        $res =  mysqli_query($connection,$q);
-        if($res){
-                while($rows = mysqli_fetch_assoc($res)){
+        $query ="SELECT tbl_teacher.teacher_id, teacher_name ,teacher_email, tbl_batch.batch_name ,tbl_teacher.teacher_img FROM `tbl_teacher` join tbl_batch on tbl_batch.batch_id = tbl_teacher.batch_info;";
+        $result =  mysqli_query($connection,$query);
+        if($result){
+                while($rows = mysqli_fetch_assoc($result)){
 
             ?>
                 <tr>
@@ -26,7 +27,11 @@
                     <td><?php echo $rows["teacher_name"]?></td>
                     <td><?php echo $rows["teacher_email"]?></td>
                     <td><?php echo $rows["batch_name"]?></td>
-                    <td><img style="height:50px; width:100px" src="<?php echo $rows["teacher_img_path"]?>"/></td>
+                    <td><img style="height:50px; width:100px" src="<?php echo $rows["teacher_img"]?>"/></td>
+                    <td><a href="edit_teacher.php?id=<?php echo $rows["teacher_id"];?>" class="btn btn-primary">Edit</a></td>
+                    <td><a href="delete_teacher.php?id=<?php echo $rows["teacher_id"];?>" class="btn btn-danger"> Delete</a>
+                   
+                  
                 </tr>
             <?php
                 }
