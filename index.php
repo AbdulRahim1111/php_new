9fac55a2dcccb39fabde2a6bf13a82ad2051a53b
Student <lab-104@aptech-mlr.edu>
2025-07-16 20:54:43 +0500

    login_added

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
+<?php 
+session_start();
+if(isset($_SESSION["username"])){
+    header("location:index.php");
+    exit();
+}
+?>
 <?php include_once "header.php"?>
 <?php include_once "footer.php"?>
