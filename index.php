54c3ddff2a4c1755000f177bc1d4a60c8d51a181
Student <lab-104@aptech-mlr.edu>
2025-07-21 19:53:04 +0500

    database added

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
+
+
+
 <?php 
 session_start();
-if(isset($_SESSION["username"])){
-    header("location:index.php");
+if(!isset($_SESSION["username"])){
+    header("location:login.php");
     exit();
 }
 ?>
+
+
+
+
 <?php include_once "header.php"?>
 <?php include_once "footer.php"?>
